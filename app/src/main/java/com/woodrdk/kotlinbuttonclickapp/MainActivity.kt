d83759ce525bd9962c4a3976e1560e9c61a41c52
Robert Wood Jr <rdrwood@gmail.com>
2019-06-25 14:49:27 -0700

    button setup and implementation

--- app/src/main/java/com/woodrdk/kotlinbuttonclickapp/MainActivity.kt
+++ app/src/main/java/com/woodrdk/kotlinbuttonclickapp/MainActivity.kt
@@ -2,18 +2,29 @@ package com.woodrdk.kotlinbuttonclickapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private var editText: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?){
+                textView?.append(userInput.text)
+                textView?.append("\n")
+            }
+        })
     }
 }
